3d6fa35f391e8dab28faa982f5f1ad913480c54a
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-12-20 21:30:07 +0530

    done

--- reduxapp/src/store/productSlice.js
+++ reduxapp/src/store/productSlice.js
@@ -13,31 +13,31 @@ const productSliceReducer = createSlice({
         data: [],
         status: STATUSES.IDLE
     },
-    // reducers: {
-    //     setProducts(state, action) {
-    //         state.data = action.payload
+    reducers: {
+        setProducts(state, action) {
+            state.data = action.payload
 
-    //     },
-    //     setStatus(state, action) {
-    //         state.status = action.payload
-    //     }
+        },
+        setStatus(state, action) {
+            state.status = action.payload
+        }
 
-    // }
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchProducts.pending, (state, action) => {
-                state.status = STATUSES.LOADING
-            
-            }).addCase(fetchProducts.fulfilled, (state, action) => {
-                
-                state.data = action.payload
-                state.status =STATUSES.IDLE
-            
-            }).addCase(fetchProducts.rejected, (state, action) => {
-                state.status = STATUSES.ERROR
-
-            })
     }
+    // extraReducers: (builder) => {
+    //     builder
+    //         .addCase(fetchProducts.pending, (state, action) => {
+    //             state.status = STATUSES.LOADING
+            
+    //         }).addCase(fetchProducts.fulfilled, (state, action) => {
+                
+    //             state.data = action.payload
+    //             state.status =STATUSES.IDLE
+            
+    //         }).addCase(fetchProducts.rejected, (state, action) => {
+    //             state.status = STATUSES.ERROR
+
+    //         })
+    // }
 
 
 })
@@ -51,10 +51,10 @@ export default productSliceReducer.reducer
 
 
 
-export const fetchProducts = createAsyncThunk("products/fetch", async () => {
-    const res = await fetch("https://fakestoreapi.com/products")
-    const data = await res.json()
-    return data
+// export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+//     const res = await fetch("https://fakestoreapi.com/products")
+//     const data = await res.json()
+//     return data
 //    return await fetch("https://fakestoreapi.com/products").then((res) =>
 
 //             res.json())
@@ -63,26 +63,26 @@ export const fetchProducts = createAsyncThunk("products/fetch", async () => {
 // return res
 //             })
     
-})
-// export function fetchProducts() {
+// })
+export function fetchProducts() {
  
 
-//     return async function fetchProductThunk(dispatch, getState) {
-//         dispatch(setStatus(STATUSES.LOADING))
-//         // const prop = getState()
+    return async function fetchProductThunk(dispatch, getState) {
+        dispatch(setStatus(STATUSES.LOADING))
+        // const prop = getState()
 
-//         await fetch("https://fakestoreapi.com/products").then((res) =>
+        await fetch("https://fakestoreapi.com/products").then((res) =>
 
-//             res.json())
-//             .then((res) => {
+            res.json())
+            .then((res) => {
 
 
-//                 dispatch(setProducts(res))
-//                 dispatch(setStatus(STATUSES.IDLE))
-//             }).catch((err) => {
-//                 console.log(err);
-//                 dispatch(setStatus(STATUSES.ERROR))
-//             })
+                dispatch(setProducts(res))
+                dispatch(setStatus(STATUSES.IDLE))
+            }).catch((err) => {
+                console.log(err);
+                dispatch(setStatus(STATUSES.ERROR))
+            })
 
-//     }
-// } 
+    }
+} 
